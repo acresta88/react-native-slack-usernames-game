757bb5a77729b34797e3b12bae8e645b494b103a
Andrea Cresta <andrea@sixty.no>
2016-03-07 10:19:23 +0100

    removed unused module

--- index.ios.js
+++ index.ios.js
@@ -36,18 +36,11 @@ var Orientation = require('react-native-orientation'); //only for ios
   
 var IntroScreen = require('./JS/Screens/IntroScreen');
 
-var Device = require('react-native-device');
 
 var FriendlyGame = React.createClass({
 
   render: function() {    
       Orientation.lockToPortrait();      
-    // if(Device.isIphone()) {
-    //   Orientation.lockToPortrait();      
-    // } 
-    // else {
-    //   Orientation.lockToLandscape();
-    // }
  
     return (
       <View style= {styles.container}>
